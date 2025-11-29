a25a2596771222f4285b02a665c7af195c956238
Donsir Arcilla <66265988+donsirr@users.noreply.github.com>
2025-11-29 13:05:03 +0800

    WIP: Resolving pre-revert conflicts/changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,8 @@ import { CartProvider } from "@/lib/cart-context"
 import { MainLayoutWrapper } from "@/components/main-layout-wrapper"
 import { UIProvider } from "@/lib/ui-context"
 import { SocketProvider } from "@/lib/socket-context"
-import { Toaster } from "@/components/ui/toaster"
-import { NotificationListener } from "@/components/notification-listener"
-import { DevToolbar } from "@/components/dev-toolbar" // NEW IMPORT
+import { Toaster } from "@/components/ui/toaster" // Import Toaster
+import { NotificationListener } from "@/components/notification-listener" // Import Listener
 
 const geist = Geist({ subsets: ["latin"] })
 const geistMono = Geist_Mono({ subsets: ["latin"] })
@@ -32,14 +31,11 @@ export default function RootLayout({
         <SocketProvider>
           <CartProvider>
             <UIProvider>
-              <NotificationListener />
+              {/* These two components enable the notification system globally */}
+              <NotificationListener /> 
               <MainLayoutWrapper>
                 {children}
               </MainLayoutWrapper>
-              
-              {/* NEW: Developer Toolbar for Role Switching */}
-              <DevToolbar />
-              
               <Toaster />
             </UIProvider>
           </CartProvider>
